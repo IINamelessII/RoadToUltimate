0a61492ed6cdbbb070f859e49167e24c374480f6
Nitro <justnitro26@gmail.com>
2018-09-14 09:52:54 +0300

    add struct

--- palevo/urls.py
+++ palevo/urls.py
@@ -4,7 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search/', views.search, name='search'),
     path('solutions/', views.solutions, name='solutions'),
+    path('teachers/', views.teachers, name='teachers'),
+    path('users/', views.users, name='users'),
+    path('subjects/', views.subjects, name='subjects'),
+    path('groups/', views.groups, name='groups'),
     path('solutions/<int:solution_id>/', views.solution, name='solution'),
+    path('teachers/<int:teacher_id>/', views.teacher, name='teacher'),
+    path('groups/<int:group_id>/', views.group, name='group'),
+    path('subjects/<int:subjects_id>/', views.subjects, name='subjects'),
+    path('users/<int:user_id>/', views.user, name='user'),
 ]
--- palevo/views.py
+++ palevo/views.py
@@ -4,26 +4,82 @@ from .models import Group, Solution, User, Teacher, Subject, Semester
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the palevo-service index.")
+    context = {
 
-
-def search(request):
-    contex = {
-        'solutions': Solution.objects.all(),
     }
-    return HttpResponse("Hello, world. You're at the search index.")
+    return render(request, 'palevo/index.html', context)
 
 
 def solutions(request):
-    contex = {
+    context = {
         'solutions': Solution.objects.all(),
     }
-    return render(request, 'palevo/solutions.html', contex)
+    return render(request, 'palevo/solutions.html', context)
+
+
+def teachers(request):
+    context = {
+        'teachers': Teacher.objects.all(),
+    }
+    return render(request, 'palevo/teachers.html', context)
+
+
+def users(request):
+    context = {
+        'users': User.objects.all(),
+    }
+    return render(request, 'palevo/users.html', context)
+
+
+def subjects(request):
+    context = {
+        'subjects': Subject.objects.all(),
+    }
+    return render(request, 'palevo/subjects.html', context)
+
+
+def groups(request):
+    context = {
+        'groups': Group.objects.all(),
+    }
+    return render(request, 'palevo/groups.html', context)
 
 
 def solution(request, solution_id):
     solution = get_object_or_404(Solution, pk=solution_id)
-    contex = {
+    context = {
         'solution': solution,
     }
-    return render(request, 'palevo/solution.html', contex)
+    return render(request, 'palevo/solution.html', context)
+
+
+def teacher(request, teacher_id):
+    teacher = get_object_or_404(Teacher, pk=teacher_id)
+    context = {
+        'teacher': teacher,
+    }
+    return render(request, 'palevo/teacher.html', context)
+
+
+def group(request, group_id):
+    group = get_object_or_404(Group, pk=group_id)
+    context = {
+        'group': group,
+    }
+    return render(request, 'palevo/group.html', context)
+
+
+def subject(request, subject_id):
+    subject = get_object_or_404(Subject, pk=subject_id)
+    context = {
+        'subject': subject,
+    }
+    return render(request, 'palevo/subject.html', context)
+
+
+def user(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    context = {
+        'user': user,
+    }
+    return render(request, 'palevo/user.html', context)
