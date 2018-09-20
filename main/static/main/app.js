a08dd174434695527f354936cc81028b9e3cd9eb
Nitro <justnitro26@gmail.com>
2018-09-21 00:40:38 +0300

    Added background slider and 1st audio

--- main/static/main/app.js
+++ main/static/main/app.js
@@ -1,10 +1,10 @@
 let images = [
-  "images/0.jpg",
-  "images/1.jpg",
-  "images/2.jpg",
-  "images/3.jpg",
-  "images/4.jpg",
-  "images/5.jpg"
+  "main/images/0.jpg",
+  "main/images/1.jpg",
+  "main/images/2.jpg",
+  "main/images/3.jpg",
+  "main/images/4.jpg",
+  "main/images/5.jpg"
   ];
 
 let num = 0;
@@ -14,7 +14,7 @@ function next() {
   if(num >= images.length) {
     num = 0;
   }
-  $("body").css("background", "url(\"" + images[num]+ "\") no-repeat");
+  $("body").css("background", "url('/static/" + images[num]+ "') no-repeat");
 }
 
 function prev() {
@@ -22,5 +22,9 @@ function prev() {
   if(num < 0) {
     num = images.length - 1;
   }
-  $("body").css("background", "url(\"" + images[num]+ "\") no-repeat");
+  $("body").css("background", "url('/static/" + images[num]+ "') no-repeat");
 }
+
+//{% static 'main/app.js' %}
+
+//$("body").css("background", "url('" + images[num]+ "') no-repeat");
